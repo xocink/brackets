6abb56e99116c08d555c4dc2e5bfaa23daab0f75
xocink <57539437+xocink@users.noreply.github.com>
2021-03-11 10:32:07 +0300

    feat : add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let exampleBracketsStr = bracketsConfig.map(b => b.join(''));
+    let inner_str = str
+    console.log(exampleBracketsStr)
+    for (let i = 0; i < exampleBracketsStr.length; i++) {
+
+        if (inner_str.includes(exampleBracketsStr[i])) {
+            inner_str = inner_str.replace(exampleBracketsStr[i], '');
+            i = -1;
+        }
+    }
+    return inner_str.length === 0;
+
 }
